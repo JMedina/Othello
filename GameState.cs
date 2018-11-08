4ab51dd8a9a4fcecbbd568263e22ea2387b136e4
Julio Medina <jamedina@hmc.edu>
2018-11-08 10:47:13 -0800

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -3,29 +3,28 @@ using System.Collections;
 
 public class GameState : Monobehaviour
 {
-	char [,] board = new char [8,8];  //col, then row
-	char whosTurn;
+   char [,] board = new char [8,8];  //col, then row
+   char whosTurn;
 
 
-	void Start () 
-	{
-		for (int col = 0; col < 8, ++col)
-		{
-			for (int row = 0, row < 8; ++row)
-			{
-				board [col][row] = ' ';
-			}
-		}
+   void Start () 
+   {
+      for (int col = 0; col < 8, ++col)
+      {
+         for (int row = 0, row < 8; ++row)
+         {
+            board [col][row] = ' ';
+         }
+      }
 
-		board [3][3] = 'w';
-		board [4][4] = 'w';
+      board [3][3] = 'w';
+      board [4][4] = 'w';
 
-		board [3][4] = 'b';
-		board [4][3] = 'w';
+      board [3][4] = 'b';
+      board [4][3] = 'b';
 
       whosTurn = 'b';
-	}
-
+   }
 
 
    void Update 
@@ -42,6 +41,7 @@ public class GameState : Monobehaviour
          for (int row = 0; row < 8; ++row)
          {
             possibleMoves [col][row] = possibleMoves(col, row, whosTurn);
+
             if (possibleMoves [col][row].Count > 0)
             {
                skipTurn = false;
@@ -52,307 +52,395 @@ public class GameState : Monobehaviour
       if (skipTurn == true)
       {
          whosTurn = (whosTurn == 'b') ? 'w' : 'b';
+
+         //TODO: tell UI that we're skipping the player's turn
          return;
       }
 
       
       //Receive input from UI and update board accordingly
+      bool successfulMove = false;
+      while (!successfulMove)
+      {
+         successfulMove = updateBoard (whosTurn, possibleMoves [col][row]);
+      }
 
+      whosTurn = (whosTurn == 'b') ? 'w' : 'b';
    }
 
-   void updateBoard (int col, int row, char whosTurn)
+
+   //Change ownership of the relevant pieces
+   bool updateBoard (char whosTurn, List<string> moves)
    {
-      
+      if (moves.Count == 0)
+      {
+         return false;
+      }
+
+
+      if (moves.Contains("N"))
+      {
+         int i = 1;
+         while (board [col][row-i] != whosTurn)
+         {
+            board [col][row-i] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
+      if (moves.Contains("S"))
+      {
+         int i = 1;
+         while (board [col][row+i] != whosTurn)
+         {
+            board [col][row+i] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
+      if (moves.Contains("E"))
+      {
+         int i = 1;
+         while (board [col+i][row] != whosTurn)
+         {
+            board [col+i][row] = whosTurn;
+            ++i;
+         }
+         return true;
+      }      
+
+      if (moves.Contains("W"))
+      {
+         int i = 1;
+         while (board [col-i][row] != whosTurn)
+         {
+            board [col-i][row] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
+      if (moves.Contains("NE"))
+      {
+         int i = 1;
+         while (board [col-i][row+i] != whosTurn)
+         {
+            board [col-i][row+i] = whosTurn;
+            ++i;
+         }
+            return true;
+      }
+
+      if (moves.Contains("NW"))
+      {
+         int i = 1;
+         while (board [col-i][row-i] != whosTurn)
+         {
+            board [col-i][row-i] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
+      if (moves.Contains("SE"))
+      {
+         int i = 1;
+         while (board [col+i][row+i] != whosTurn)
+         {
+            board [col+i][row+i] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
+      if (moves.Contains("SW"))
+      {
+         int i = 1;
+         while (board [col+i][row-i] != whosTurn)
+         {
+            board [col+i][row-i] = whosTurn;
+            ++i;
+         }
+         return true;
+      }
+
    }
 
 
 
+   //For column and row passed in from UI, determines if move possible
+   public List<str>  isPossible (int col, int row, char whosTurn)
+   {
+      string possibleMoves = [];
+
+      if (whosTurn == 'b')
+      {
+         //check up
+         if (row-1 >= 0 && board [col][row-1] == 'w')
+         {
+            for (int i = 2; (row - i) >= 0; ++i)
+            {
+               if (board [col][row-i] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col][row-i] == 'b')
+               {
+                  possibleMoves.Add ("N");
+                  break;
+               }
+            }
+         }
+
+         //check down
+         if (row+1 < 8 && board [col][row+1] == 'w')
+         {
+            for (int i = 2; (row + i) < 8; ++i)
+            {
+               if (board [col][row+i] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col][row+i] == 'b')
+               {
+                  possibleMoves.Add ("S");
+                  break;
+               }
+            }
+         }
+
+         //check left
+         if (col-1 >= 0 && board [col-1][row] == 'w')
+         {
+            for (int i = 2; (col - i) >= 0; ++i)
+            {
+               if (board [col-i][row] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col-i][row] == 'b')
+               {
+               possibleMoves.Add ("W");
+                  break;
+               }
+            }
+         }
+
+         //check right
+         if (col+1 < 8 && board [col+1][row] == 'w')
+         {
+            for (int i = 2; (row + i) < 8 ; ++i)
+            {
+               if (board [col+i][row] == 'w')
+               {
+                   continue;
+               }
+               else if (board [col+i][row] == 'b')
+               {
+                  possibleMoves.Add ("E");
+                  break;
+               }
+            }
+         }
 
 
-	//For column and row passed in from UI, determines if move possible
-	public List<str>  isPossible (int col, int row, char whosTurn)
-	{
-		string possibleMoves = [];
+         //check NW
+         if (col-1 >= 0  && row-1 >= 0 && board [col-1][row-1] == 'w')
+         {
+            dist = Math.min(row, col);
 
-		if (whosTurn == 'b')
-		{
-			//check up
-			if (row-1 >= 0 && board [col][row-1] == 'w')
-			{
-				for (int i = 2; (row - i) >= 0; ++i)
-				{
-					if (board [col][row-i] == 'w')
-					{
-						continue;
-					}
-					else if (board [col][row-i] == 'b')
-					{
-						possibleMoves.Add ("N");
-						break;
-					}
-				}
-			}
+            for (int i = 2; (dist - i) >= 0; ++i)
+            {
+               if (board [col-i][row-i] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col-i][row-i] == 'b')
+               {
+                  possibleMoves.Add ("NW");
+                  break;
+               }
+            }
+         }
 
-			//check down
-			if (row+1 < 8 && board [col][row+1] == 'w')
-			{
-				for (int i = 2; (row + i) < 8; ++i)
-				{
-					if (board [col][row+i] == 'w')
-					{
-						continue;
-					}
-					else if (board [col][row+i] == 'b')
-					{
-						possibleMoves.Add ("S");
-						break;
-					}
-				}
-			}
+         //check NE
 
-			//check left
-			if (col-1 >= 0 && board [col-1][row] == 'w')
-			{
-				for (int i = 2; (col - i) >= 0; ++i)
-				{
-					if (board [col-i][row] == 'w')
-					{
-						continue;
-					}
-					else if (board [col-i][row] == 'b')
-					{
-						possibleMoves.Add ("W");
-						break;
-					}
-				}
-			}
+         //check SW
+         if (col-1 >= 0 && row+1 < 8 && board [col-1][row+1] == 'w')
+         {
+            dist = Math.min(col, 8-row);
 
-			//check right
-			if (col+1 < 8 && board [col+1][row] == 'w')
-			{
-				for (int i = 2; (row + i) < 8 ; ++i)
-				{
-					if (board [col+i][row] == 'w')
-					{
-						continue;
-					}
-					else if (board [col+i][row] == 'b')
-					{
-						possibleMoves.Add ("E");
-						break;
-					}
-				}
-			}
+            for (int i = 2; (dist - i) >= 0; ++i)
+            {
+               if (board [col-i][row+i] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col-i][row+i] == 'b')
+               {
+                  possibleMoves.Add ("SW");
+                  break;
+               }
+            }
+         }
 
+         //check SE
+         if (col+1 < 8 && row+1 < 8 && board [col+1][row+1] == 'w')
+         {
+            dist = Math.max(row, col);
 
-			//check NW
-			if (col-1 >= 0  && row-1 >= 0 && board [col-1][row-1] == 'w')
-			{
-				dist = Math.min(row, col);
+            for (int i = 2; (dist + i) < 8; ++i)
+            {
+               if (board [col+i][row+i] == 'w')
+               {
+                  continue;
+               }
+               else if (board [col+i][row+i] == 'b')
+               {
+                  possibleMoves.Add ("SE");
+                  break;
+               }
+            }
+         }
+      }
+         
 
+      else if (whosTurn == 'w')
+      {
+         //check up
+         if (row-1 >= 0 && board [col][row-1] == 'b')
+         {
+            for (int i = 2; (row - i) >= 0; ++i)
+            {
+               if (board [col][row-i] == 'b')
+               {
+                  continue;
+               }
+               else if (board [col][row-i] == 'w')
+               {
+                  possibleMoves.Add ("N");
+                  break;
+               }
+            }
+         }
 
-				for (int i = 2; (dist - i) >= 0; ++i)
-				{
-					if (board [col-i][row-i] == 'w')
-					{
-						continue;
-					}
-					else if (board [col-i][row-i] == 'b')
-					{
-						possibleMoves.Add ("NW");
-						break;
-					}
-				}
-			}
+         //check down
+         if (row+1 < 8 && board [col][row+1] == 'b')
+         {
+            for (int i = 2; (row + i) < 8; ++i)
+            {
+               if (board [col][row+i] == 'b')
+               {
+                  continue;
+               }
+               else if (board [col][row+i] == 'w')
+               {
+                  possibleMoves.Add ("S");
+                  break;
+               }
+            }
+         }
 
-			//check NE
+               //check left
+               if (col-1 >= 0 && board [col-1][row] == 'b')
+               {
+                        for (int i = 2; (col - i) >= 0; ++i)
+                        {
+                                 if (board [col-i][row] == 'b')
+                                 {
+                                          continue;
+                                 }
+                                 else if (board [col-i][row] == 'w')
+                                 {
+                                          possibleMoves.Add ("W");
+                                          break;
+                                 }
+                        }
+               }
 
-			//check SW
-			if (col-1 >= 0 && row+1 < 8 && board [col-1][row+1] == 'w')
-			{
-				dist = Math.min(col, 8-row);
-
-				for (int i = 2; (dist - i) >= 0; ++i)
-				{
-					if (board [col-i][row+i] == 'w')
-					{
-						continue;
-					}
-					else if (board [col-i][row+i] == 'b')
-					{
-						possibleMoves.Add ("SW");
-						break;
-					}
-				}
-			}
-
-			//check SE
-			if (col+1 < 8 && row+1 < 8 && board [col+1][row+1] == 'w')
-			{
-				dist = Math.max(row, col);
-
-				for (int i = 2; (dist + i) < 8; ++i)
-				{
-					if (board [col+i][row+i] == 'w')
-					{
-						continue;
-					}
-					else if (board [col+i][row+i] == 'b')
-					{
-						possibleMoves.Add ("SE");
-						break;
-					}
-				}
-			}
-
-		}
-	
-
-		else if (whosTurn == 'w')
-		{
-			//check up
-			if (row-1 >= 0 && board [col][row-1] == 'b')
-			{
-				for (int i = 2; (row - i) >= 0; ++i)
-				{
-					if (board [col][row-i] == 'b')
-					{
-						continue;
-					}
-					else if (board [col][row-i] == 'w')
-					{
-						possibleMoves.Add ("N");
-						break;
-					}
-				}
-			}
-
-			//check down
-			if (row+1 < 8 && board [col][row+1] == 'b')
-			{
-				for (int i = 2; (row + i) < 8; ++i)
-				{
-					if (board [col][row+i] == 'b')
-					{
-						continue;
-					}
-					else if (board [col][row+i] == 'w')
-					{
-						possibleMoves.Add ("S");
-						break;
-					}
-				}
-			}
-
-			//check left
-			if (col-1 >= 0 && board [col-1][row] == 'b')
-			{
-				for (int i = 2; (col - i) >= 0; ++i)
-				{
-					if (board [col-i][row] == 'b')
-					{
-						continue;
-					}
-					else if (board [col-i][row] == 'w')
-					{
-						possibleMoves.Add ("W");
-						break;
-					}
-				}
-			}
-
-			//check right
-			if (col+1 < 8 && board [col+1][row] == 'b')
-			{
-				for (int i = 2; (row + i) < 8 ; ++i)
-				{
-					if (board [col+i][row] == 'b')
-					{
-						continue;
-					}
-					else if (board [col+i][row] == 'w')
-					{
-						possibleMoves.Add ("E");
-						break;
-					}
-				}
-			}
+               //check right
+               if (col+1 < 8 && board [col+1][row] == 'b')
+               {
+                        for (int i = 2; (row + i) < 8 ; ++i)
+                        {
+                                 if (board [col+i][row] == 'b')
+                                 {
+                                          continue;
+                                 }
+                                 else if (board [col+i][row] == 'w')
+                                 {
+                                          possibleMoves.Add ("E");
+                                          break;
+                                 }
+                        }
+               }
 
 
-			//check NW
-			if (col-1 >= 0  && row-1 >= 0 && board [col-1][row-1] == 'b')
-			{
-				dist = Math.min(row, col);
+               //check NW
+               if (col-1 >= 0  && row-1 >= 0 && board [col-1][row-1] == 'b')
+               {
+                        dist = Math.min(row, col);
 
 
-				for (int i = 2; (dist - i) >= 0; ++i)
-				{
-					if (board [col-i][row-i] == 'b')
-					{
-						continue;
-					}
-					else if (board [col-i][row-i] == 'w')
-					{
-						possibleMoves.Add ("NW");
-						break;
-					}
-				}
-			}
+                        for (int i = 2; (dist - i) >= 0; ++i)
+                        {
+                                 if (board [col-i][row-i] == 'b')
+                                 {
+                                          continue;
+                                 }
+                                 else if (board [col-i][row-i] == 'w')
+                                 {
+                                          possibleMoves.Add ("NW");
+                                          break;
+                                 }
+                        }
+               }
 
-			//check NE
+               //check NE
 
-			//check SW
-			if (col-1 >= 0 && row+1 < 8 && board [col-1][row+1] == 'b')
-			{
-				dist = Math.min(col, 8-row);
+               //check SW
+               if (col-1 >= 0 && row+1 < 8 && board [col-1][row+1] == 'b')
+               {
+                        dist = Math.min(col, 8-row);
 
-				for (int i = 2; (dist - i) >= 0; ++i)
-				{
-					if (board [col-i][row+i] == 'b')
-					{
-						continue;
-					}
-					else if (board [col-i][row+i] == 'w')
-					{
-						possibleMoves.Add ("SW");
-						break;
-					}
-				}
-			}
+                        for (int i = 2; (dist - i) >= 0; ++i)
+                        {
+                                 if (board [col-i][row+i] == 'b')
+                                 {
+                                          continue;
+                                 }
+                                 else if (board [col-i][row+i] == 'w')
+                                 {
+                                          possibleMoves.Add ("SW");
+                                          break;
+                                 }
+                        }
+               }
 
-			//check SE
-			if (col+1 < 8 && row+1 < 8 && board [col+1][row+1] == 'b')
-			{
-				dist = Math.max(row, col);
+               //check SE
+               if (col+1 < 8 && row+1 < 8 && board [col+1][row+1] == 'b')
+               {
+                        dist = Math.max(row, col);
 
-				for (int i = 2; (dist + i) < 8; ++i)
-				{
-					if (board [col+i][row+i] == 'b')
-					{
-						continue;
-					}
-					else if (board [col+i][row+i] == 'w')
-					{
-						possibleMoves.Add ("SE");
-						break;
-					}
-				}
-			}
-			
-		}
+                        for (int i = 2; (dist + i) < 8; ++i)
+                        {
+                                 if (board [col+i][row+i] == 'b')
+                                 {
+                                          continue;
+                                 }
+                                 else if (board [col+i][row+i] == 'w')
+                                 {
+                                          possibleMoves.Add ("SE");
+                                          break;
+                                 }
+                        }
+               }
+               
+      }
 
-		return possibleMoves;
-	}
-
-
-	void flipPices (int col, int row, char whosTurn)
-	{
-		List possibleMoves = possibleMoves (col, row, whosTurn);
-
-		foreach (string direction in possibleMoves)
-		{
-
-		}
-	}
+                  return possibleMoves;
+         }
 
 }
